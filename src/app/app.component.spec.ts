78ce327e5a8ad41b71414f9678ab2b222e70ba7d
courtney4150 <courtneyrsheridan@gmail.com>
2021-07-08 15:33:57 -0400

    last default unit test works

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('angular-tour-of-heroes');
   });
 });
