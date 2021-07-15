0e5882da70e1758c6246c61af2ce30863462ed88
courtney4150 <courtneyrsheridan@gmail.com>
2021-07-15 13:45:06 -0400

    unique images on All Heroes page

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Dr Nice',      photo: "https://img.icons8.com/carbon-copy/2x/image.png"},
+    { id: 12, name: 'Narco',        photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 13, name: 'Bombasto',     photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 14, name: 'Celeritas',    photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 15, name: 'Magneta',      photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 16, name: 'RubberMan',    photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 17, name: 'Dynama',       photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 18, name: 'Dr IQ',        photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 19, name: 'Magma',        photo: "https://img.icons8.com/carbon-copy/2x/image.png" },
+    { id: 20, name: 'Tornado',      photo: "https://img.icons8.com/carbon-copy/2x/image.png" }
 ]
